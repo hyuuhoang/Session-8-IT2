b414755f841d5c07dd13c483d678e001fb3965a8
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-11-29 00:51:48 +0700

    nguyen huy hoang

---
+++ session 8  IT2 bài 6.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int mang[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    int tong = 0;
+    int n = 4;
+
+    printf("Cac phan tu tren duong cheo chinh la: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", mang[i][i]);
+        tong += mang[i][i];
+    }
+
+    printf("\nTong cac phan tu tren duong cheo chinh la: %d\n", tong);
+
+    return 0;
+}
+
